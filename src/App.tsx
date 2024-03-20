4049ebb2f449dd23fd3bc23078510a9a06cf8667
lhg99 <lee991229@naver.com>
2024-03-20 18:55:41 +0900

    오피스아워 자료

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import './App.css'
+import { CreateNoteModal, TagsModal } from './components'
+import { useAppSelector } from './hooks/redux'
+import { Navbar, Sidebar } from './layout'
+import { AllNotes, ArchiveNotes, ErrorPage, TagNotes, TrashNotes } from './pages'
+import 'react-toastify/dist/ReactToastify.css'
+
 
 function App() {
-  
+
+  const { viewEditTagsModal, viewCreateNoteModal } = useAppSelector(state => state.modal);
+
   return (
-    <div className="App">
-      <div>
-        
-      </div>
+    <div className="app">
+
+      {viewCreateNoteModal && <CreateNoteModal />}
+      {viewEditTagsModal && <TagsModal type='edit' />}
+
+      <ToastContainer
+        position='bottom-right'
+        theme='light'
+        pauseOnHover
+        autoClose={1500}
+      />
+
+      <BrowserRouter>
+        <Sidebar />
+        <div className='app__container'>
+          <Navbar /> 
+          <Routes>
+            <Route path='/' element={<AllNotes />} />
+            <Route path='/archive' element={<ArchiveNotes />} />
+            <Route path='/trash' element={<TrashNotes />} />
+            <Route path='/tag/:name' element={<TagNotes />} />
+            <Route path='/404' element={<ErrorPage />} />
+            <Route path='/*' element={<Navigate to={"/404"} />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
